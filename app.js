5912ab9ab31e3a05899c228028d533e7b563835f
Julian Tobal <juliantobal27@hotmail.com>
2022-01-24 19:45:24 -0300

    carrito

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ app.get('/',(req,res)=>{
 app.get('/register',(req, res)=>{
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
 });
+
+app.get('/cart',(req, res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/cart.html'))
+});
