221e385a314868d90c3d4490a2e938f08ef1ae46
grpalma <fliapalmareyes@gmail.com>
2022-05-30 15:45:57 -0300

    creacion directorio migrations y archivos create: brand, product and user

---
+++ database/migrations/create_product.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { FOREIGNKEYS } = require("sequelize/types/lib/query-types");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('product', {
+      product_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: dataTypes.INTEGER
+      },
+      product_name: {
+        type: dataTypes.STRING
+      },
+      
+      product_price: {
+        type: dataTypes.DECIMAL
+      },
+      product_brand_id: {
+        type: dataTypes.INTEGER
+     },
+      product_user_id: {
+        type: dataTypes.INTEGER
+        
+      }
+      
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('product');
+  }
+};
+
